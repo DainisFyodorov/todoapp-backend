4ab6baeef012802362ea1bbdd4c6fb885900dfa2
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-17 23:59:46 +0200

    Initial commit

---
+++ src/main/java/lv/dainis/todoapp/TodoappApplication.java
@@ -0,0 +1,13 @@
+package lv.dainis.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/dainis/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.dainis.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
