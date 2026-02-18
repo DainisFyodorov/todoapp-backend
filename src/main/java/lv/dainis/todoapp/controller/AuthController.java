86183016fa5d8a1a5bce3f1ee7483b2037bb228f
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-18 22:54:58 +0200

    AuthController "/api/auth/status" endpoint implemented for getting status of the session for user

--- src/main/java/lv/dainis/todoapp/controller/AuthController.java
+++ src/main/java/lv/dainis/todoapp/controller/AuthController.java
@@ -1,14 +1,16 @@
 package lv.dainis.todoapp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lv.dainis.todoapp.entity.User;
 import lv.dainis.todoapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,5 +27,14 @@ public class AuthController {
     public ResponseEntity<Void> register(@Valid @RequestBody User user) {
         userService.registerUser(user);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Map<String, Object>> checkStatus(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session != null && session.getAttribute("user") != null) {
+            return ResponseEntity.ok(Map.of("isLoggedIn", true));
+        }
+        return ResponseEntity.ok(Map.of("isLoggedIn", false));
     }
 }
