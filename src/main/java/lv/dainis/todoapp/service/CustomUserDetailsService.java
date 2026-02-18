12bf4ad7498e33fb47ebf3789850349dd2d7a929
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-18 11:39:53 +0200

    CustomUserDetailsService implemented

---
+++ src/main/java/lv/dainis/todoapp/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package lv.dainis.todoapp.service;
+
+import lombok.NonNull;
+import lv.dainis.todoapp.dao.UserRepository;
+import lv.dainis.todoapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
