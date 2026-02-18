7b78be12905f967e08d82c6903e3fc602c8821a4
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-18 12:56:41 +0200

    TaskRepository findAllByUser implemented

--- src/main/java/lv/dainis/todoapp/dao/TaskRepository.java
+++ src/main/java/lv/dainis/todoapp/dao/TaskRepository.java
@@ -1,7 +1,12 @@
 package lv.dainis.todoapp.dao;
 
 import lv.dainis.todoapp.entity.Task;
+import lv.dainis.todoapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByUser(User user);
 }
