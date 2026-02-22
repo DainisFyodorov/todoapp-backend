18a50e2d7429a75f02d48545d704b8b61f00845b
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-22 23:47:10 +0200

    CategoryRepository findAllByUser added

--- src/main/java/lv/dainis/todoapp/dao/CategoryRepository.java
+++ src/main/java/lv/dainis/todoapp/dao/CategoryRepository.java
@@ -1,7 +1,12 @@
 package lv.dainis.todoapp.dao;
 
 import lv.dainis.todoapp.entity.Category;
+import lv.dainis.todoapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByUser(User user);
 }
