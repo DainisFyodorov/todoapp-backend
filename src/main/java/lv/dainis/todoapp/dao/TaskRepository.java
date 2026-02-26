a2619e3a984833c30e8a8a62775d057364abdd07
DainisFyodorov <fedorovdainis@gmail.com>
2026-02-26 11:09:55 +0200

    TaskRepository clearTasksFromCategory SQL query implemented

--- src/main/java/lv/dainis/todoapp/dao/TaskRepository.java
+++ src/main/java/lv/dainis/todoapp/dao/TaskRepository.java
@@ -3,10 +3,20 @@ package lv.dainis.todoapp.dao;
 import lv.dainis.todoapp.entity.Task;
 import lv.dainis.todoapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findAllByUser(User user);
+
+    @Modifying
+    @Query("""
+        update Task t 
+        set t.category = null 
+        where t.category.id = :categoryId
+    """)
+    void clearTasksFromCategory(Long categoryId);
 }
