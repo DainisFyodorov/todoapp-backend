fa023aebd89da67747632524accf53a549c0e271
DainisFyodorov <fedorovdainis@gmail.com>
2026-03-03 11:33:30 +0200

    CustomOAuth2UserServiceTest newUserTest unit test implemented

--- src/main/java/lv/dainis/todoapp/service/CustomOAuth2UserService.java
+++ src/main/java/lv/dainis/todoapp/service/CustomOAuth2UserService.java
@@ -2,9 +2,11 @@ package lv.dainis.todoapp.service;
 
 import lv.dainis.todoapp.dao.UserRepository;
 import lv.dainis.todoapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -14,17 +16,25 @@ import java.util.Collections;
 import java.util.UUID;
 
 @Service
-public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
+    private final DefaultOAuth2UserService delegate;
 
+    @Autowired
     public CustomOAuth2UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.delegate = new DefaultOAuth2UserService();
+    }
+
+    public CustomOAuth2UserService(UserRepository userRepository, DefaultOAuth2UserService delegate) {
+        this.userRepository = userRepository;
+        this.delegate = delegate;
     }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String username = oAuth2User.getAttribute("email");
 
---
+++ src/test/java/lv/dainis/todoapp/service/CustomOAuth2UserServiceTest.java
@@ -0,0 +1,67 @@
+package lv.dainis.todoapp.service;
+
+import lv.dainis.todoapp.dao.UserRepository;
+import lv.dainis.todoapp.entity.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomOAuth2UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private OAuth2UserRequest oAuth2UserRequest;
+
+    @Mock
+    private DefaultOAuth2UserService defaultOAuth2UserService;
+
+    @InjectMocks
+    private CustomOAuth2UserService customOAuth2UserService;
+
+    @Test
+    void newUserTest() {
+        String email = "dainis@gmail.com";
+        Map<String, Object> attributes = Map.of("email", email);
+        OAuth2User mockOAuth2User = new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("USER")),
+                attributes,
+                "email"
+        );
+
+        when(defaultOAuth2UserService.loadUser(oAuth2UserRequest))
+                .thenReturn(mockOAuth2User);
+
+        when(userRepository.findByUsername(email)).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenAnswer(i -> i.getArgument(0));
+
+        OAuth2User result = customOAuth2UserService.loadUser(oAuth2UserRequest);
+
+        assertNotNull(result);
+        assertEquals(email, result.getAttribute("email"));
+        assertTrue(result.getAuthorities().stream()
+                .anyMatch(a -> Objects.equals(a.getAuthority(), "USER")));
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
