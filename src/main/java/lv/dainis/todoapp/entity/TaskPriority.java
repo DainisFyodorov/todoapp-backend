ae728664e77ef2a0a9222fdad2b88dc36027abc3
DainisFyodorov <fedorovdainis@gmail.com>
2026-03-13 13:50:00 +0200

    TaskPriority validation improved

--- src/main/java/lv/dainis/todoapp/entity/TaskPriority.java
+++ src/main/java/lv/dainis/todoapp/entity/TaskPriority.java
@@ -1,7 +1,20 @@
 package lv.dainis.todoapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TaskPriority {
     LOW,
     MEDIUM,
-    HIGH
+    HIGH;
+
+    @JsonCreator
+    public static TaskPriority fromString(String value) {
+        for(TaskPriority taskPriority : TaskPriority.values()) {
+            if(taskPriority.name().equalsIgnoreCase(value)) {
+                return taskPriority;
+            }
+        }
+
+        return null;
+    }
 }
